c05b1e9f6bdfde4b634e524e50bff08a509d3cda
Ivan Alvarado <ivalvara21@gmail.com>
2021-07-26 21:48:23 -0700

    Init commit

---
+++ backend/src/main/kotlin/com/ivanalvarado/Application.kt
@@ -0,0 +1,11 @@
+package com.ivanalvarado
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.ivanalvarado.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
