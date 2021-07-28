6af2dc72fb60b9a02eb6591e31c25cb55e256a50
Ivan Alvarado <ivalvara21@gmail.com>
2021-07-27 20:26:19 -0700

    Add a new route example
    More info: https://ktor.io/docs/routing-in-ktor.html

--- backend/src/main/kotlin/com/ivanalvarado/Application.kt
+++ backend/src/main/kotlin/com/ivanalvarado/Application.kt
@@ -10,7 +10,15 @@ fun main(args: Array<String>): Unit = EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     routing {
         get("/") {
-            call.respondText("Hello, from module!")
+            call.respondText("Hello, from home!")
         }
+
+        newRoute()
+    }
+}
+
+private fun Route.newRoute() {
+    get("/hello") {
+        call.respondText("Hello, from new route")
     }
 }
