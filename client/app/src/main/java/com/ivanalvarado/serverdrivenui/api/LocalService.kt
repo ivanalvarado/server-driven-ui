0706acc04fbae2f2e8776e8ca2e6b9fd15189126
Ivan Alvarado <ivalvara21@gmail.com>
2021-07-29 21:31:14 -0700

    Add service and api model

--- client/app/src/main/java/com/ivanalvarado/serverdrivenui/api/LocalService.kt
+++ client/app/src/main/java/com/ivanalvarado/serverdrivenui/api/LocalService.kt
@@ -1,4 +1,10 @@
 package com.ivanalvarado.serverdrivenui.api
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface LocalService {
+    @GET("/theme/{theme}")
+    suspend fun getTheme(@Path("theme") theme: String): Response<ThemeResponse>
 }
--- client/app/src/main/java/com/ivanalvarado/serverdrivenui/api/ThemeResponse.kt
+++ client/app/src/main/java/com/ivanalvarado/serverdrivenui/api/ThemeResponse.kt
@@ -1,3 +1,19 @@
 package com.ivanalvarado.serverdrivenui.api
 
-data class ThemeResponse()
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ThemeResponse(
+    @Json(name = "primary") val primary: Color?,
+    @Json(name = "secondary") val secondary: Color?,
+    @Json(name = "background") val background: Color?,
+    @Json(name = "surface") val surface: Color?,
+    @Json(name = "onPrimary") val onPrimary: Color?,
+    @Json(name = "onBackground") val onBackground: Color?,
+    @Json(name = "onSurface") val onSurface: Color?
+)
+
+data class Color(
+    @Json(name = "value") val value: Int
+)
