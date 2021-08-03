07800d2a3c85dba858d080995632c6cbbd44f650
Ivan Alvarado <ivalvara21@gmail.com>
2021-08-02 21:48:52 -0700

    Add a composable that displays a color and its name

--- client/app/src/main/java/com/ivanalvarado/serverdrivenui/MainActivity.kt
+++ client/app/src/main/java/com/ivanalvarado/serverdrivenui/MainActivity.kt
@@ -3,16 +3,32 @@ package com.ivanalvarado.serverdrivenui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ivanalvarado.serverdrivenui.ui.theme.ServerDrivenUITheme
+import com.ivanalvarado.serverdrivenui.viewmodel.ThemeViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: ThemeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ServerDrivenUITheme {
                 // A surface container using the 'background' color from the theme
@@ -22,6 +38,22 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun ColorRow(colorName: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
+        Box(modifier = Modifier.background(color = Color.Blue).size(50.dp, 50.dp))
+        Spacer(Modifier.size(12.dp))
+        Text(text = colorName)
+    }
+}
+
+@Preview
+@Composable
+fun ColorRowPreview() {
+    ColorRow("Primary")
 }
 
 @Composable
